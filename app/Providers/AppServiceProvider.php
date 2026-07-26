6507d0c224e83b6b4d4fbb21483d40969e0d02ba
josed1231 <josedavidzuluagabarco@gmail.com>
2026-07-25 23:53:40 -0500

    Registrado driver Brevo usando Transport::fromDsn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        // Registrar el driver de Brevo API mediante DSN estático
+        Mail::extend('brevo', function (array $config = []) {
+            return Transport::fromDsn(
+                'brevo+api://' . config('services.brevo.key') . '@default'
+            );
+        });
     }
 }
